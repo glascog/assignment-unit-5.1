15b2e7c93f9d3cba49fb1008358dc14251f62848
Gabe Glasco <gabeglasco@gmail.com>
2023-07-04 19:55:32 -0500

    added numbers 1 and 2

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -10,6 +10,11 @@ console.log('***** Object Practice *****')
 */
 const me = {
   // TODO - add properties here
+  firstName: 'Gabrielle',
+  lastName: 'Glasco',
+  hasSiblings: true,
+  shoeCount: 7,
+  favThreeFoods: ['tacos', 'curry', 'pizza']
 };
 console.log('A little about me:', me);
 
@@ -19,7 +24,8 @@ console.log('A little about me:', me);
     created above to set its value. 
   - Console.log fullName
 */
-
+const fullName = `${me.firstName} ${me.lastName}`;
+console.log(fullName);
 
 
 /* 3. Nested arrays
